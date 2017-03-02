400669cf32bb0aa830071420748ea3f22ddf1a29
Chr96er <christianhomberg@gmail.com>
2017-03-02 11:43:19 +1000

    - add renderVersion

--- R/shinyUtilities.R
+++ R/shinyUtilities.R
@@ -21,3 +21,26 @@ getDeploymentDate <- function(){
   deploymentDatetime <- strsplit(strsplit(gsub(".VERSION", replacement = "", dir()[grep(dir(), pattern = "VERSION")]), "-")[[1]][2],"#")[[1]]
   paste0(strftime(strptime(deploymentDatetime[1], format = "%Y.%m.%d.%H.%M.%S"),format = "%Y.%m.%d %H:%M:%S")," ", gsub("\\.", "/", deploymentDatetime[2]))
 }
+
+#'@export
+#'@param url Url to source code repository
+#'@param version Version vector containing four integers
+#'@param deploymentDate Date formatted as String
+renderVersion <- function(url,version,deploymentDate){
+  list(
+    tags$p(
+      paste0("Source code available under ", url),
+      align = "right"
+    ),
+    tags$p(
+      "Version ",
+      paste(version[1:3], collapse = "."),
+      "; build ",
+      version[4],
+      " (last deployment: ",
+      deploymentDate,
+      ")",
+      align = "right"
+    )
+  )
+}
